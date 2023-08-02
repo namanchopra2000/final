6de3fbf633995f426508d485c125bdfb3160d0a0
namanchopra2000 <117727607+namanchopra2000@users.noreply.github.com>
2023-08-02 17:51:09 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import { BrowserRouter as Router , Routes , Route } from 'react-router-dom';
+import SingupSingIn from './pages/singup';
+import Dashboard from './pages/dashboard';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import MyComponent from './Componenets/mycomponent';
+
+function App() {
+  return (
+    <>
+     <ToastContainer />
+      <Router>
+        <Routes>
+          <Route path='/' element={<SingupSingIn/>}/>
+          <Route path='/dashboard' element={<Dashboard/>} />
+        </Routes>
+      </Router>
+    </>
+    
+    
+  );
+}
+
+export default App;
